95122927094b626b6775473500a778d07ca70d1f
Facundo Lista <faculista2@gmail.com>
2022-12-15 16:45:21 -0300

    Modificaciones en comentarios

--- src/main/java/com/obligatorio2dda/obligatorio2dda/service/ClientService.java
+++ src/main/java/com/obligatorio2dda/obligatorio2dda/service/ClientService.java
@@ -6,9 +6,9 @@ import com.obligatorio2dda.obligatorio2dda.entity.Client;
 public interface ClientService {
 
     public Iterable<Client> findAll();  // Buscar Todos los clientes
-    public Client findById(Long Ci);    // Buscar por id
+    public Client findById(Long Ci);    // Buscar por ci
     public Client save(Client client);  // Guardar el cliente
-    public void deleteById(Long Ci);    // Borrar uno por id
+    public void deleteById(Long Ci);    // Borrar uno por ci
     public Page<Client> findAll(Pageable pageable); // Buscar todos en el pageObject
 
 }
